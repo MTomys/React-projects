8a530ce94211f879eacd1a4ff5634216cad7cce3
MTomys <michalltomys@protonmail.ch>
2022-06-29 13:02:39 +0200

    Add arrays solution

---
+++ typescript-practice/typescript-features/arrays.ts
@@ -0,0 +1,31 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+// const carMakers: string[] = ['ford', 'toyota', 'chevy'];
+
+const dates = [new Date(), new Date()];
+
+// 2d arrays
+const carsByMake = [['f150'], ['corolla'], ['camaro']];
+
+// or
+// carsByMake: string[][] = [];
+
+const toyota = carMakers[1];
+
+const myCar = carMakers.pop();
+
+// prevents incompatible values
+// carMakers.push(5);
+
+console.log(carMakers);
+// map
+const carsMapped = carMakers.map((car: string): string => {
+  return car + ' mapped.';
+});
+
+console.log(carsMapped);
+
+// Flexible types
+const importantDates = [new Date(), '2030-10-10'];
+
+importantDates.push('2020-123-123');
+importantDates.push(new Date());
