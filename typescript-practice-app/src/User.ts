0195b14a87936c06b3cdce630467a4543a18b25b
MTomys <michalltomys@protonmail.ch>
2022-06-29 22:27:07 +0200

    Add typescript practice app

---
+++ typescript-practice-app/src/User.ts
@@ -0,0 +1,20 @@
+// Create and export class = capital name for a file
+import { faker } from '@faker-js/faker';
+
+// never use default
+
+export class User {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
